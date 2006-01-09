cc1c7465027b4de916b74d7e51dcd7eaa5dfa1a7
phil <phil@33d97d02-e2f8-0310-af11-9b3162d49f83>
2006-01-09 02:59:59 +0000

    clean up a little, and make the first jumped to thing a script
    git-svn-id: http://svn.hands.com/d-i@73 33d97d02-e2f8-0310-af11-9b3162d49f83

--- dsd/0-first.sh
+++ dsd/0-first.sh
@@ -1,13 +1,17 @@
-# first.cfg preseed from http://hands.com/d-i/dsd-subst/first.cfg
+#!/bin/sh
+# 0-first.sh preseed from http://hands.com/d-i/dsd/0-first.cfg
 #
 # Copyright (c) 2005 Hands.com Ltd
 # distributed under the terms of the GNU GPL version 2 or (at your option) any later version
 # see the file "COPYING" for details
 #
-# This looks like it could have been put in the preseed2.cfg file, but
-# that would not allow the variables set in the tests to be used here
+# It is likely, that with the advent of the new dashslashdash substitution
+# aproach, with executable preseed scripts, I'll be able to reduce the nummber
+# of seprate config files, but for the moment, I'll leave it pretty much as
+# it is apart from making this one executable for later flexibility
 #
 
+cat <<'!EOF!'
 # here we put stuff that we expect to override
 d-i     languagechooser/language-name   string English
 d-i     countrychooser/shortlist        string US
@@ -19,4 +23,9 @@ base-config     apt-setup/hostname      string ftp.debian.org
 base-config     apt-setup/directory     string /debian
 
 d-i preseed/include     string class_setup.sh|
+
+# This line is currently only including second.cfg -- I'm hoping to work out
+# something interesting to allow for more flexible subclassing using the
+# subclass.sh files -- this needs some inspiration first
 d-i preseed/include_command     string ( echo "domain-$(debconf-get netcfg/get_domain)/subclass.sh|?"; IFS=';'; for cls in $(debconf-get local/classes); do [ "$cls" ] && echo "${cls}/subclass.sh|?"; done ; echo second.cfg )
+!EOF!
