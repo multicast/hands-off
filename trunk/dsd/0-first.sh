bf27b9a2e9d2d526b1ec8b174e97285a895d96d5
phil <phil@33d97d02-e2f8-0310-af11-9b3162d49f83>
2006-02-18 09:23:02 +0000

    opps, forgot the main dsd bit ... try again
    git-svn-id: http://svn.hands.com/d-i@88 33d97d02-e2f8-0310-af11-9b3162d49f83

---
+++ trunk/dsd/0-first.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# 0-first.sh preseed from http://hands.com/d-i/dsd/0-first.cfg
+#
+# Copyright (c) 2005 Hands.com Ltd
+# distributed under the terms of the GNU GPL version 2 or (at your option) any later version
+# see the file "COPYING" for details
+#
+# It is likely, that with the advent of the new dashslashdash substitution
+# aproach, with executable preseed scripts, I'll be able to reduce the nummber
+# of seprate config files, but for the moment, I'll leave it pretty much as
+# it is apart from making this one executable for later flexibility
+#
+
+cat <<'!EOF!'
+# here we put stuff that we expect to override
+d-i     languagechooser/language-name   string English
+d-i     countrychooser/shortlist        string US
+d-i     console-keymaps-at/keymap       string us
+d-i     mirror/country                  string enter information manually
+d-i     mirror/http/hostname            string ftp.debian.org
+d-i     mirror/http/directory           string /debian
+base-config     apt-setup/hostname      string ftp.debian.org
+base-config     apt-setup/directory     string /debian
+
+d-i preseed/include     string class_setup.sh|
+
+# This line is currently only including second.cfg -- I'm hoping to work out
+# something interesting to allow for more flexible subclassing using the
+# subclass.sh files -- this needs some inspiration first
+d-i preseed/include_command     string echo second.cfg
+
+### previous try ...
+#d-i preseed/include_command     string ( echo "domain-$(debconf-get netcfg/get_domain)/subclass.sh|?"; IFS=';'; for cls in $(debconf-get local/classes); do [ "$cls" ] && echo "${cls}/subclass.sh|?"; done ; echo second.cfg )
+!EOF!
---
+++ trunk/dsd/class_setup.sh
@@ -0,0 +1,45 @@
+# This needs some work.
+# At present, it just allows you to tack classes onto the front of the current
+# class list -- I feel that there is probably a need to allow the new classes
+# to be added at the start of the list, just before or after the one that
+# depended on them, or at the end of the list, but until I see examples
+# where that extra complication is actually required, lets just do this
+#
+# BTW this uses the perhaps dodgy trick of relying on being run by something
+# that provides the function preseed_fetch, to grab the subclass files
+#
+
+set -x
+
+split_semi() {
+  echo "$1" | sed -e 's/;/\n/g'
+}
+
+join_semi() {
+  tr '\n' ';' | sed -e 's/;$//'
+}
+
+subclasses() {
+   preseed_fetch "$(dirname $last_location)/${1}/subclasses" /tmp/cls-$1 || return 0
+   join_semi < /tmp/cls-$1
+   rm /tmp/cls-$1
+}
+
+expandclasses() {
+  for c in $(split_semi $1) ; do
+    expandclasses $(subclasses $c)
+  done
+  for c in $(split_semi $1) ; do
+    echo $c
+  done
+}
+
+sieve() {
+  read x || return
+  echo $x
+  sieve | grep -v "^$x$"
+}
+
+classes="$(debconf-get local/classes)"
+
+echo local local/classes string $(expandclasses "$classes" | sieve | join_semi)
