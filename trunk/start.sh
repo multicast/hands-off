ed525552b1c8fc26c00dccda7b4af10285882b91
phil <phil@33d97d02-e2f8-0310-af11-9b3162d49f83>
2006-07-27 14:09:10 +0000

    allow this to work when not using local/
    git-svn-id: http://svn.hands.com/d-i@183 33d97d02-e2f8-0310-af11-9b3162d49f83

--- trunk/start.sh
+++ trunk/start.sh
@@ -10,7 +10,7 @@ set -e
 . /usr/share/debconf/confmodule
 
 preseed_fetch local_enabled_flag /tmp/local_enabled_flag
-use_local=$(grep -v '^[[:space:]]*\(#\|$\)' /tmp/local_enabled_flag)
+use_local=$(grep -v '^[[:space:]]*\(#\|$\)' /tmp/local_enabled_flag || true)
 rm /tmp/local_enabled_flag
 echo $use_local > /var/run/hands-off.local
 if [ "true" = "$use_local" ]
