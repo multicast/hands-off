2e26e199fb4be653315aa2d6e54d218c4f46dde7
Philip Hands <phil@hands.com>
2014-09-23 22:28:29 +0100

    copy files back to squeeze

---
+++ squeeze/etch.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# etch.sh preseed from http://hands.com/d-i/.../etch.sh
+#
+# Copyright (c) 2008 Hands.com Ltd
+# distributed under the terms of the GNU GPL version 2 or (at your option) any later version
+# see the file "COPYING" for details
+#
+set -e
+
+. /usr/share/debconf/confmodule
+. /tmp/HandsOff-fn.sh
+
+checkflag dbg/pauses all etch && pause "Top Level etch.sh script 1"
+
+# Etch was more verbose here
+if db_get mirror/country && [ "manual" = "$RET" ] ; then
+  db_set mirror/country "enter information manually"
+fi
+
+# in Lenny you leave this unset, although that breaks on multi-disk systems
+if ! db_get partman-auto/disk || [ -z "$RET" ] ; then
+  db_really_set partman-auto/disk /dev/discs/disc0/disc true
+fi
