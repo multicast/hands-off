f7e8dac4fa20e6ed4d5934ebf8d36054d912e3ab
Daniel Dehennin <daniel.dehennin@baby-gnu.org>
2016-04-16 16:33:37 +0200

    Factorize “early_command” and “late_command” runners
    Use the same logic and code to execute per classes
    “preseed/early_command” and “preseed/late_command”.
    
    This will avoid duplicating logic for new command hooks like
    “partition/early_command”.
    
    The new script requires 3 parameters:
    
    1. an ID used to build class script name and URL, ending in “_script”
    2. a description to display in dialog boxes
    3. a semi-colon separated list of name on which to pause, a “-x” is
       automatically append to each one for debug.
    
    Each script can access its class name by the exported “${class}”
    environment variable.
    
    * utils/HandsOff-classes-scripts-runner.sh: New script implementing the
      common logic between “classes/early_script” and “classes/late_script”.
    
    * start.sh: Download new per class scripts runner and make it executable.
    
    * start.cfg: Replace calls to “classes/early_script” and
      “classes/late_script” by the new per class scripts runner.

---
+++ preseed/utils/HandsOff-classes-scripts-runner.sh
@@ -0,0 +1,105 @@
+#!/bin/sh
+#
+# Copyright © 2013-2016 Daniel Dehennin <daniel.dehennin@baby-gnu.org>
+#
+# Distributed under the terms of the GNU GPL version 2 or (at your
+# option) any later version see the file "COPYING" for details
+
+set -e
+
+. /tmp/HandsOff-fn.sh
+
+echo "debug: Running..."
+
+PAUSE_TITLE='Per class scripts runner'
+RUNNER_ID="${1}"
+RUNNER_DESC="${2}"
+RUNNER_FLAGS="${3}"
+
+if [ -z "${RUNNER_ID}" -o -z "${RUNNER_DESC}" -o -z "${RUNNER_FLAGS}" ]
+then
+	error "Missing some arguments:
+RUNNER_ID: ${RUNNER_ID}
+RUNNER_DESC: ${RUNNER_DESC}
+RUNNER_FLAGS: ${RUNNER_FAGS}"
+	handsoff_poweroff
+fi
+
+pause_flags() {
+	echo all $(split_semi "${RUNNER_FLAGS}")
+}
+
+debug_flags() {
+	local flags
+	for flag in $(pause_flags)
+	do
+		flags="${flags} ${flag}-x"
+	done
+	echo ${flags}
+}
+
+checkflag dbg/flags all-x runner-x $(debug_flags) \
+    && set -x
+checkflag dbg/pauses all runner $(pause_flags) \
+    && pause "Run ${RUNNER_DESC} scripts" "${PAUSE_TITLE}"
+
+db_register hands-off/meta/text hands-off/title
+db_register hands-off/meta/text hands-off/item
+
+db_subst hands-off/title DESC "Preseed ${RUNNER_ID}_script(s)"
+db_progress START 0 $({ echo myself; classes; } | wc -w) hands-off/title
+
+db_subst hands-off/item DESC "Running top level ${RUNNER_DESC} script"
+db_progress INFO hands-off/item
+
+db_progress STEP 1
+# Chain onto class specific script(s) if any.
+for class in $(classes)
+do
+	db_subst hands-off/item DESC \
+		 "${RUNNER_DESC} script for class “${class}”"
+	db_progress INFO hands-off/item
+
+	if expr "${class}" : virtual/ >/dev/null; then
+		echo "debug: no load of virtual “${cls}”"
+		db_progress STEP 1
+		continue
+	elif expr "${class}" : local/ >/dev/null; then
+		CLASS_SCRIPT_URL="/${class}/${RUNNER_ID}_script"
+	else
+		CLASS_SCRIPT_URL="/classes/${class}/${RUNNER_ID}_script"
+	fi
+
+	cl_a_ss=$(shell_escape "${class}")
+	if ! preseed_fetch "${CLASS_SCRIPT_URL}" \
+	     "/tmp/${RUNNER_ID}_script-${cl_a_ss}"
+	then
+		echo "warning: ...${CLASS_SCRIPT_URL} not found"
+		# Remove possible empty file
+		rm -f "/tmp/${RUNNER_ID}_script-${cl_a_ss}"
+	fi
+
+	if [ -e "/tmp/${RUNNER_ID}_script-${cl_a_ss}" ]
+	then
+		checkflag dbg/pauses $(pause_flags) \
+		    && pause "Running ${RUNNER_DESC} script for “${class}”" \
+			     "${PAUSE_TITLE}"
+
+		# Scripts can get their class name with “${class}”
+		export class
+
+		chmod +x "/tmp/${RUNNER_ID}_script-${cl_a_ss}"
+		log-output -t "${class}/${RUNNER_ID}_script" \
+			   "/tmp/${RUNNER_ID}_script-${cl_a_ss}"
+	fi
+	db_progress STEP 1
+done
+
+db_progress STOP
+db_unregister hands-off/item
+db_unregister hands-off/title
+
+checkflag dbg/pauses all runner $(pause_flags) \
+    && pause "End ${RUNNER_DESC} scripts" "${PAUSE_TITLE}"
+
+echo "debug: completed successfully"
