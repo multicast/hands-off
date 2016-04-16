2df4f77e348db2eaaea29b4e849f7a676709084b
Daniel Dehennin <daniel.dehennin@baby-gnu.org>
2016-04-16 16:08:17 +0200

    Rearrange “preseed/start.sh”
    Make sure “auto-install/classes” is initialised first.

--- preseed/start.sh
+++ preseed/start.sh
@@ -38,6 +38,13 @@ checkflag dbg/flags all-x start-x && set -x
 
 check_udeb_ver preseed-common 1.29 || backcompat=etch.sh
 
+# Make sure that auto-install/classes exists, even if it wasn't on the cmdline
+db_get auto-install/classes || {
+  db_register hands-off/meta/string auto-install/classes/title
+  db_register hands-off/meta/string auto-install/classes
+  db_subst auto-install/classes ID auto-install/classes
+}
+
 # Local classes can be set by the local_enabled_flag file
 # or by hands-off/local=(true|false) command line option
 preseed_fetch local_enabled_flag /var/run/hands-off.local || true
@@ -52,13 +59,6 @@ then
 else
   db_set preseed/run subclass.sh $backcompat
 fi
-
-# Make sure that auto-install/classes exists, even if it wasn't on the cmdline
-db_get auto-install/classes || {
-  db_register hands-off/meta/string auto-install/classes/title
-  db_register hands-off/meta/string auto-install/classes
-  db_subst auto-install/classes ID auto-install/classes
-}
 
 # kludge to deal with breakage in Jessie
 if [ -e /var/run/preseed_unspecified_at_boot ] &&
