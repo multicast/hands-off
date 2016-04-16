46702b8f91da11f6b50a2c7fc615f739f6f67861
Daniel Dehennin <daniel.dehennin@baby-gnu.org>
2016-04-16 15:37:52 +0200

    Move “HandsOff-fn.sh” to “utils/”
    The HandsOff utilities library became quite big and deserves to be in
    its own file.
    
    This makes the editors syntax colorization and automatic indentation
    working.
    
    * preseed/start.sh: Remove creation of “/tmp/HandsOff-fn.sh” and download
      it with “preseed_fetch()”.
    
    * preseed/utils/HandsOff-fn.sh: Import code from “start.sh”.
      Consistently quote variables and indent file.

---
+++ preseed/utils/HandsOff-fn.sh
@@ -0,0 +1,72 @@
+# Common utilities
+#
+# Copyright (c) 2005-2006 Hands.com Ltd
+# Copyright © 2012-2016 Daniel Dehennin <daniel.dehennin@baby-gnu.org>
+#
+# Distributed under the terms of the GNU GPL version 2 or (at your
+# option) any later version see the file "COPYING" for details
+#
+# The following utilities are usable by early and late scripts, they
+# just need to source it:
+#
+#     . /tmp/HandsOff-fn.sh
+#
+. /usr/share/debconf/confmodule
+. /lib/preseed/preseed.sh
+
+# useful functions for preseeding
+in_class() {
+	echo ";$(debconf-get auto-install/classes);" | grep -q ";${1};"
+}
+classes() {
+	echo "$(debconf-get auto-install/classes)" | sed 's/;/\n/g'
+}
+checkflag() {
+	local flagname="${1}" ; shift
+	if db_get "${flagname}" && [ "${RET}" ]
+	then
+		for i in "${@}"; do
+			echo ";${RET};" | grep -q ";${i};" && return 0
+		done
+	fi
+	return 1
+}
+pause() {
+	local desc="${1}"
+
+	db_register hands-off/meta/text hands-off/pause/title
+	db_subst hands-off/pause/title DESC "Conditional Debugging Pause"
+	db_settitle hands-off/pause/title
+
+	db_register hands-off/meta/text hands-off/pause
+	db_subst hands-off/pause DESCRIPTION "${desc}"
+	db_input critical hands-off/pause
+	db_unregister hands-off/pause
+	db_unregister hands-off/pause/title
+	db_go
+}
+
+# db_set fails if the variable is not already registered -- this gets round that
+# this might need to check if the variable already exits
+db_really_set() {
+	local var="${1}"
+	local val="${2}"
+	local seen="${3}"
+
+	db_register debian-installer/dummy "${var}"
+	db_set "${var}" "${val}"
+	db_subst "${var}" ID "${var}"
+	db_fset "${var}" seen "${seen}"
+}
+
+# Tools
+check_udeb_ver() {
+	# returns true if the udeb is at least Version: ver
+	local udeb="${1}"
+	local ver="${2}"
+
+	{ echo "${ver}" ;
+	  sed -ne '/^Package: '${udeb}'$/,/^$/s/^Version: \(.*\)$/\1/p' \
+	      /var/lib/dpkg/status ;
+	} | sort -t. -c 2>/dev/null
+}
