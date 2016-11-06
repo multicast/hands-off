9a89a4ba518edee5a8289fe2dcb2b514fccf2ac8
Philip Hands <phil@hands.com>
2016-11-06 20:39:21 +0100

    create /bin/preseed_lookup_checksum if needed (plus other tweaks)

--- jessie/checksigs.sh
+++ jessie/checksigs.sh
@@ -14,20 +14,33 @@ set -x
 db_get hands-off/checksigs && checksigs="$RET"
 
 if [ "true" = "$checksigs" ] ; then
+	sums=/var/lib/preseed/checksums-md5sum
+	lookup=/bin/preseed_lookup_checksum
+
 	# Should be adding a checksum from a signed checksum file here
 	for f in MD5SUMS.asc trustedkeys.gpg ; do
 		preseed_fetch $f /tmp/$f
 	done
-	sums=/tmp/MD5SUMS.asc
+	tmp_sums=/tmp/MD5SUMS.asc
 	keys=/tmp/trustedkeys.gpg
 
-	# let's see if this gets us past the gpgv call below
-	modprobe rng-core
+	# let's see if this gets us past the gpgv call below (which eats entropy) -- this is a pathetic kludge, but seems to do the trick
+	ping -c 100 $(ip route | sed -n '/^default via \([.0-9]*\) .*$/\1/p') > /dev/null &
+
 	# FIXME: we need some way to bootstrap this trust, since anyone could add their key to this downloaded file
-	gpgv --keyring $keys $sums
+	gpgv --keyring $keys $tmp_sums || exit 1
 
-	mv $keys /var/lib/preseed/checksums-md5sum
+	mv $tmp_sums $sums
 
-	db_set preseed/include/checksum $(sed -ne '/ \.[/]start.cfg$/s/ .*//p' $sums)
+	if [ ! -f $lookup ] ; then
+		# if the lookup script is missing, add our own
+		cat > $lookup <<-!EOF!
+			#!/bin/sh -e
+			grep " \$1$" $sums | cut -d\  -f1)
+			!EOF!
+		chmod +x $lookup
+	fi
+
+	db_set preseed/include/checksum $($lookup start.cfg)
 fi
 db_set preseed/include start.cfg
