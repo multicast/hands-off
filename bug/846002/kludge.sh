3c3e9f9d4902658f208f20a560c48665d44bf5a8
Philip Hands <phil@hands.com>
2016-12-12 02:36:05 +0100

    bug/846002: add debugging for tasksel-debconf

--- bug/846002/kludge.sh
+++ bug/846002/kludge.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
-postinst=/var/lib/dpkg/info/pkgsel.postinst
-# this is a joyous race condition
-while [ ! -e $postinst ] ; do
+f=/var/lib/dpkg/info/pkgsel.postinst
+# replace pkgsel's postinst before it gets invoked
+while [ ! -e $f ] ; do
 	sleep 1
 done
 
-mv -f /tmp/pkgsel.postinst $postinst
+mv -f /tmp/pkgsel.postinst $f
+
+# put some debugging into the tasksel's debconf helper
+f=/target/usr/lib/tasksel/tasksel-debconf
+while [ ! -e $f ] ; do
+        sleep 1
+done
+
+sed -i '/^db_go/a echo "[DEBUG-846002]: continuing after db_go -- if you get to here after having pressed [BACK] then db_go did not return 30 as it should -- which is a bug AFAICS" >&2' $f
